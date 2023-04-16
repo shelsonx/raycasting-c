ea84b5b099a20ee7eef50f1d8c9cc4a984db51ca
shelson <shelsonx@gmail.com>
2023-04-16 03:03:53 -0300

    render windown and draw rect

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 build:
-	gcc -std=c99 ./src/*.c -ISLD2 -o raycast
+	gcc -std=c99 ./src/*.c -ISLD2 -o raycast $$(sdl2-config --cflags --libs)
 
 
 run:
---
+++ src/constants.h
@@ -0,0 +1,9 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define FALSE 0
+#define TRUE 1
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#endif
